7f1f9e0bee5e0bed36dae964d9cc7da66e3ecb0b
dejan.randjelovic <dejan.randjelovic@appmodule.net>
2023-01-19 00:37:32 +0100

    Added: env variables loaded in bash.

--- backend/NodeServices/startStoreService.sh
+++ backend/NodeServices/startStoreService.sh
@@ -1,1 +1,6 @@
-cd EmployeeStoreService && dapr run --app-id employeestoreservice --app-port 9080 --dapr-http-port 3500 --components-path ../../../dapr/components -- npm start
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+cd EmployeeStoreService && npm i && dapr run --app-id employeestoreservice --app-port $STORE_SERVICE_HTTP_PORT --dapr-http-port $DAPR_DATABASE_PORT --components-path ../../../dapr/components -- npm start
